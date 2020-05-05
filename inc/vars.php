23961be1f35fb031392c9a0275be65d717f25717
RubenArca <64140248+RubenArca@users.noreply.github.com>
2020-05-05 15:09:01 +0200

    Actualizar variables

---
+++ inc/vars.php
@@ -0,0 +1,27 @@
+<?php
+  // Ruta local
+  /*$urlabsoluta = "./";
+  $servername = "localhost";
+  $username = "root";
+  $password = "";
+  $database = "arca21";*/
+
+  // Ruta web
+  $urlabsoluta = "./";
+  $servername = "PMYSQL127.dns-servicio.com:3306";
+  $username = "ruben";
+  $password = "m4btQ#54";
+  $database  = "7442508_aresresidencial";
+
+  //Nombre de la web
+  $web = "Arca XXI";
+
+  //Traer variables de NAV
+  require($urlabsoluta . $url . "inc/varnav.php");
+
+  //Traer variables del HOME
+  require($urlabsoluta . $url . "inc/homevar.php");
+
+  //Traer variables del FILE
+  require($urlabsoluta . $url . "inc/filevar.php");
+?>
